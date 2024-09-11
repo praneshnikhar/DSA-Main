6d6131c0f110bdfaf6c9924758c7c4c34c7a6ccb
praneshnikhar <praneshnikhar@gmail.com>
2024-09-11 10:22:00 +0530

    initial commit for this project

---
+++ src/calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.println("enter the operator");
+            char op = sc.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("enter two numbers");
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                if (op == '+') {
+                    ans = a + b;
+                }
+                if (op == '-') {
+                    ans = a - b;
+
+                }
+                if (op == '*') {
+                    ans = a * b;
+                }
+                if (op == '/') {
+                    if (b != 0) {
+                        ans = a / b;
+                    } else {
+                        System.out.println("not defined");
+                    }
+                    if (op == '%') {
+                        ans = a % b;
+                    }
+
+                } else if (op == 'x' || op == 'X') {
+                    break;
+                } else {
+                    System.out.println("invlaid Operator");
+                }
+                System.out.println(ans);
+            }
+
+        }
+    }
+}
