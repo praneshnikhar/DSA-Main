82ffa460b63d32eefda82a69c06c7ab64415aea6
praneshnikhar <praneshnikhar@gmail.com>
2024-09-22 21:40:30 +0530

    initial commit for this project2

--- src/searchInStrings.java
+++ src/searchInStrings.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class searchInStrings {
     public static void main(String[] args) {
         String name = "Kunal";
         char target ='u';
-        System.out.println(search(name, target));
-
+//        System.out.println(search(name, target));
+        System.out.println(Arrays.toString(name.toCharArray()));
     }
+
     static boolean search(String str, char target){
         if(str.length() == 0){
             return false;
