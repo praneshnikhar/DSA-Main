0a71fe1d63ca52b7e3d3d0a42b3ad55b42250389
praneshnikhar <praneshnikhar@gmail.com>
2024-11-10 12:03:25 +0530

    initial commit for this project2

---
+++ src/sumOfDigit.java
@@ -0,0 +1,22 @@
+public class sumOfDigit {
+    public static void main(String[] args) {
+    int ans = sum(1342);
+    int ans1 = prod(55);
+        System.out.println(ans);
+        System.out.println(ans1);
+    }
+    static int sum(int n ){
+        if( n ==0 ){
+            return 0;
+        }
+        return (n%10) + sum(n/10);
+    }
+
+    static int prod(int n ){
+        if( n%10 ==n ){
+            return 0;
+        }
+        return (n%10) * sum(n/10);
+    }
+
+}
