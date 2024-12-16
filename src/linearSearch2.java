afb94bd1285e0ec704775d4e8194b2b93491ee29
praneshnikhar <praneshnikhar@gmail.com>
2024-12-16 22:25:30 +0530

    initial commit for this project2

---
+++ src/linearSearch2.java
@@ -0,0 +1,12 @@
+public class linearSearch2 {
+    public static void main(String[] args) {
+
+    }
+    int linearSearch(int arr[], int n , int x){
+        for (int i = 0 ; i <n;i++){
+            if(arr[i] == x){}
+            return i;
+        }
+    return -1;
+    }
+}
