dab2c224e7b03b0eb80dee8a86696e9c5d24bdc8
praneshnikhar <praneshnikhar@gmail.com>
2025-05-27 00:16:47 +0530

    initial commit for this project2

---
+++ src/scope.java
@@ -0,0 +1,12 @@
+public class scope {
+    public static void main(String[] args) {
+        int a= 20;
+        int b = 10;
+        System.out.println(num);
+
+    }
+    static  void random(){
+        int num = 67;
+        System.out.println(num);
+    }
+}
