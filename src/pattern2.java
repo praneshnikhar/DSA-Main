2e1351317c52b29d88dd95c77278fa932abcae58
praneshnikhar <praneshnikhar@gmail.com>
2025-06-11 21:18:57 +0530

    Your commit message

--- src/pattern2.java
+++ src/pattern2.java
@@ -1,20 +1,13 @@
-//****
-//****
-//****
-//****
 public class pattern2 {
     public static void main(String[] args) {
-        pattern1(4);
+        pattern2(4);
     }
-
-    static void pattern1(int n) {
-        for (int row = 1; row <= n; row++) {
-            // For every row, run the column
-            for (int col = 1; col <= n; col++) { // Change < to <=
-                System.out.print("* "); // Use print to stay on the same line
+    static void pattern2(int n ){
+        for(int row = 1; row <= n; row++ ){
+            for(int col = 1; col <=n; col++){
+                System.out.print("* ");
             }
-            // When one row is printed, add a new line
-            System.out.println(); // This will print a new line after each row
+            System.out.println();
         }
     }
 }
--- src/pattern3.java
+++ src/pattern3.java
@@ -1,22 +1,13 @@
-//*****
-//****
-//***
-//**
-//*
-
 public class pattern3 {
     public static void main(String[] args) {
         pattern3(4);
     }
-
-    static void pattern3(int n) {
-        for (int row = 1; row <= n; row++) {
-            // For every row, run the column
-            for (int col = 1; col <= n-row+1; col++) { // Change < to <=
-                System.out.print("* "); // Use print to stay on the same line
+    static void pattern3(int n ){
+        for(int row = n; row > 0; row-- ){
+            for(int col = 1 ; col <= row; col++){
+                System.out.print("* ");
             }
-            // When one row is printed, add a new line
-            System.out.println(); // This will print a new line after each row
+            System.out.println();
         }
     }
 }
