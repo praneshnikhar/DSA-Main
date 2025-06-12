ef107cf91039ba276f743df0e35d9fc04315019b
praneshnikhar <praneshnikhar@gmail.com>
2025-06-12 16:18:05 +0530

    commit

--- src/pattern6.java
+++ src/pattern6.java
@@ -1,12 +1,14 @@
 public class pattern6 {
     public static void main(String[] args) {
-
+        pattern6(5);
     }
     static void pattern6(int n){
         for (int row = 0; row < 2 *n ; row++) {
             int totalColsInRow = row > n?2 *n -row : row;
-            int numberSpaces = 456;
-            for(int s= 0; s <numberSpaces)
+            int numberSpaces = n - totalColsInRow;
+            for(int s= 0; s <numberSpaces; s++){
+                System.out.print(" ");
+            }
             for (int col = 0 ; col < totalColsInRow; col++){
                 System.out.print("* ");
             }
