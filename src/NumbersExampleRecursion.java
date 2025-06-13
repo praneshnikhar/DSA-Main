d06f9efdd2b8d1c4fee556a38a34a77c6597f13d
praneshnikhar <praneshnikhar@gmail.com>
2025-06-13 23:35:40 +0530

    commit

--- src/NumbersExampleRecursion.java
+++ src/NumbersExampleRecursion.java
@@ -27,8 +27,8 @@ public class NumbersExampleRecursion {
         if(n==5){
             System.out.println(5);
             return;
-
         }
+
         System.out.println(n);
         print(n+1);
     }
