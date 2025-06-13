3e72bac59748317a22be31f7d48e6078cea97d7e
praneshnikhar <praneshnikhar@gmail.com>
2025-06-13 13:16:26 +0530

    commit

---
+++ src/recursion.java
@@ -0,0 +1,10 @@
+public class recursion {
+    public static void main(String[] args) {
+        //write a fucntion that prints helo world
+        message();
+    }
+    static void message(){
+        System.out.println("hello world");
+    }
+
+}
