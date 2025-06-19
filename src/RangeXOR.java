13fe5dd729e6283c8df37f8e4f599f19caccc3c3
praneshnikhar <praneshnikhar@gmail.com>
2025-06-19 17:49:52 +0530

    commit

--- src/RangeXOR.java
+++ src/RangeXOR.java
@@ -2,9 +2,9 @@ public class RangeXOR {
     public static void main(String[] args) {
         int a = 3;
         int b = 9;
-        int ans = xor(b) ^ xor(a-1);
+//        int ans = xor(b) ^ xor(a-1);
 
-        System.out.println(ans);
+//        System.out.println(ans);
 
         int ans2= 0;
         for(int i = a; i <= b; i++){
