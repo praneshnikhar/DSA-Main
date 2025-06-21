0b18aa24c1feea5ffe6801378829cde330946602
praneshnikhar <praneshnikhar@gmail.com>
2025-06-22 01:16:01 +0530

    commit

---
+++ src/Seive.java
@@ -0,0 +1,15 @@
+public class Seive {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        System.out.println(primes[0]);
+        sieve(n, primes);
+    }
+    static void sieve(int n, boolean[] primes){
+        for(int i = 2; i*i<=n;i++){
+            if(!primes[i]){
+
+            }
+        }
+    }
+}
