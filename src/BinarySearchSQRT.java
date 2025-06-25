edbd114a32544e00f548fb732f9ee53fd6879f44
praneshnikhar <praneshnikhar@gmail.com>
2025-06-25 14:58:24 +0530

    Seive

--- src/BinarySearchSQRT.java
+++ src/BinarySearchSQRT.java
@@ -27,7 +27,7 @@ public class BinarySearchSQRT {
         double incr = 0.1;
         for (int i = 0; i < p; i++) {
             while(root*root <= n){
-                root += incr;
+                root +=incr;
             }
             root -= incr;
             incr /= 10;
