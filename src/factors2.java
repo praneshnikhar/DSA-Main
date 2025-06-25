771a985abe7387fae3647f78c0a93d684e22f142
praneshnikhar <praneshnikhar@gmail.com>
2025-06-25 16:48:27 +0530

    Seive

--- src/factors2.java
+++ src/factors2.java
@@ -1,18 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class factors2 {
     public static void main(String[] args) {
         factors2(20);
     }
 
     static void factors2(int n ){
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 1; i <= Math.sqrt(n); i++){
             if(n%i ==0){
                 if(n/i == i){
-                    System.out.println(i);
+                    System.out.println(i+ " ");
                 }else{
                     System.out.println(i + " " + n/i);
+                    list.add(n/i);
                 }
 
             }
+        }
+        for (int i = list.size() - 1 ;i>=0 ; i--) {
+            System.out.println(list.get(i)+ " ");
         }
     }
 }
