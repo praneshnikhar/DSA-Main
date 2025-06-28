593e3bb2218b18c20018b08c2cdf22c72e4c7df6
praneshnikhar <praneshnikhar@gmail.com>
2025-06-28 20:25:16 +0530

    sorted

---
+++ src/find.java
@@ -0,0 +1,12 @@
+public class find {
+    public static void main(String[] args) {
+        int[] arr
+    }
+
+    static boolean find(int[] arr, int target, int index){
+        if(index == arr.length){
+            return false;
+        }
+        return arr[index] == target || find(arr, target, index+1);
+    }
+}
