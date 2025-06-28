d8bc229ea1f5a48410cfdb72f3aff516d0f803bf
praneshnikhar <praneshnikhar@gmail.com>
2025-06-28 20:28:05 +0530

    find element and return index value

--- src/find.java
+++ src/find.java
@@ -1,6 +1,10 @@
 public class find {
     public static void main(String[] args) {
-        int[] arr
+        int[] arr = {1,2,3,4,5,6,7};
+        System.out.println(find(arr, 5, 0));
+        System.out.println(findIndex(arr, 5, 0));
+        System.out.println(find(arr, 5, 0));
+
     }
 
     static boolean find(int[] arr, int target, int index){
@@ -9,4 +13,17 @@ public class find {
         }
         return arr[index] == target || find(arr, target, index+1);
     }
+
+    static int findIndex(int[] arr, int target, int index){
+        if(index == arr.length){
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+        else{
+            return findIndex(arr, target, index+1);
+        }
+    }
+
 }
