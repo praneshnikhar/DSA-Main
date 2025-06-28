e9b8ee8875ea6c053a830e1041a1c86d43efe81b
praneshnikhar <praneshnikhar@gmail.com>
2025-06-28 20:15:11 +0530

    sorted

--- src/sorted.java
+++ src/sorted.java
@@ -1,6 +1,6 @@
 public class sorted {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,15,6,7};
+        int[] arr = {1,2,3,4,5,6,7};
         System.out.println(sorted(arr, 0));
     }
 
