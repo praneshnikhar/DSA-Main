00ce0a9ff6f77ab921d0e1d227a690970319df5d
praneshnikhar <praneshnikhar@gmail.com>
2025-07-03 17:48:45 +0530

    RotatedBinaryS

--- src/triangle.java
+++ src/triangle.java
@@ -1,6 +1,7 @@
 public class triangle {
     public static void main(String[] args) {
         triangle(4,0);
+        triangle2(4,0);
     }
     static void triangle(int r , int c){
         if(r ==0){
@@ -14,6 +15,26 @@ public class triangle {
         else{
             System.out.println();
             triangle(r-1,0);
+        }
+    }
+
+
+
+    static void triangle2(int r , int c){
+        if(r ==0){
+            return;
+        }
+
+        if(c<r){
+
+            triangle2(r,c+1);
+            System.out.print("*");
+
+        }
+        else{
+            triangle2(r-1,0);
+            System.out.println();
+
         }
     }
 }
