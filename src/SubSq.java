0690050bfde8a5831f3395b452f26efb19b99435
praneshnikhar <praneshnikhar@gmail.com>
2025-07-05 23:55:03 +0530

    skip apple from the string

--- src/SubSq.java
+++ src/SubSq.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class SubSq {
     public static void main(String[] args) {
-        subseq("", "abc");
+//        subseq("", "abc");
+        System.out.println(subSeqReturn("", "abc"));
+
     }
     static void subseq(String p , String up){
         if(up.isEmpty()){
@@ -12,4 +16,22 @@ public class SubSq {
         subseq(p+ch, up.substring(1));
         subseq(p, up.substring(1));
     }
+
+
+
+    static ArrayList<String> subSeqReturn(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSeqReturn(p+ch, up.substring(1));
+        ArrayList<String> right = subSeqReturn(p, up.substring(1));
+        left.addAll(right);
+        return left;
+
+    }
+
 }
