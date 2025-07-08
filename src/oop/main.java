132f74d1d64525c77a7428cb357ee2490f057cbb
praneshnikhar <praneshnikhar@gmail.com>
2025-07-08 20:27:34 +0530

    commit

---
+++ src/oop/main.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class main {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        String[] names= new String[5];
+
+        Student[] students = new Student[5];
+    }
+
+    class Student{
+        int[] rno = new int[5];
+        String[] name= new String[5];
+        float[] marks = new float[5];
+    }
+}
