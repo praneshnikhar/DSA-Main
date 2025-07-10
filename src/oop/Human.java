d613997c7ced4df545eeca9a556b509c2ebb55bf
praneshnikhar <praneshnikhar@gmail.com>
2025-07-10 13:37:35 +0530

    commit

---
+++ src/oop/Human.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Human {
+    int age;
+    String name;
+    boolean married;
+    int salary;
+
+    public Human(int age, String name, boolean married,int salary) {
+        this.age = age;
+        this.name = name;
+        this.married = married;
+        this.salary = salary;
+    }
+
+}
