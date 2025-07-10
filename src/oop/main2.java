f0d00fc940c5d8ccc95a5b6208fc394bcaba586b
praneshnikhar <praneshnikhar@gmail.com>
2025-07-10 17:27:21 +0530

    commit

---
+++ src/oop/main2.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class main2 {
+    public static void main(String[] args) {
+        Human pranesh= new Human(22, "pranesh" , true, 4000000);
+        System.out.println(pranesh.name);
+
+    }
+}
