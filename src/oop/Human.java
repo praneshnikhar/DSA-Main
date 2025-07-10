9dd3a462124fd20f33423980cd9ae623faaef564
praneshnikhar <praneshnikhar@gmail.com>
2025-07-10 17:27:15 +0530

    commit

--- src/oop/Human.java
+++ src/oop/Human.java
@@ -5,12 +5,13 @@ public class Human {
     String name;
     boolean married;
     int salary;
+    static long population;
 
-    public Human(int age, String name, boolean married,int salary) {
+    public Human(int age, String name, boolean married, int salary) {
         this.age = age;
         this.name = name;
         this.married = married;
         this.salary = salary;
+        population += 1;
     }
-
 }
