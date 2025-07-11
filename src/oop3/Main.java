9423d4e483a988625279d29db8124b40f134c51b
praneshnikhar <praneshnikhar@gmail.com>
2025-07-11 13:22:18 +0530

    commit

--- src/oop3/Main.java
+++ src/oop3/Main.java
@@ -2,8 +2,10 @@ package oop3;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box(5);
-        System.out.println(box.l + " " + box.w+ " "+ box.h);
+//        Box box = new Box(5);
+//        System.out.println(box.l + " " + box.w+ " "+ box.h);
 
+        BoxWeight box2 = new BoxWeight();
+        System.out.println(box2.h+" "+box2.weight);
     }
 }
