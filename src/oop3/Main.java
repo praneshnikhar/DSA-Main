12387d2ccd53c52be9f7b7513c515d2b78427f40
praneshnikhar <praneshnikhar@gmail.com>
2025-07-11 14:12:46 +0530

    commit

--- src/oop3/Main.java
+++ src/oop3/Main.java
@@ -4,9 +4,17 @@ public class Main {
     public static void main(String[] args) {
 //        Box box = new Box(5);
 //        System.out.println(box.l + " " + box.w+ " "+ box.h);
+//
+//        BoxWeight box2 = new BoxWeight();
+//        BoxWeight box3 = new BoxWeight(2,3,4,5);
+//        System.out.println(box2.h+" "+box2.weight);
 
-        BoxWeight box2 = new BoxWeight();
-        BoxWeight box3 = new BoxWeight(2,3,4,5);
-        System.out.println(box2.h+" "+box2.weight);
+        Box box5 = new BoxWeight(2,3,4,5);
+        System.out.println(box5);
+        //there are many variables in both parent and child classes
+        //you are given acces to variables that are in the ref type i.e boxweight
+        ///hence, you should have access to weight variable
+        BoxWeight box6 = new Box(2,3,4);
+        System.out.println(box6);
     }
 }
