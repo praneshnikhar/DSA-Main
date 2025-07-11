7d4c81243f01d56886c6f040039cc3cf06fdae9e
praneshnikhar <praneshnikhar@gmail.com>
2025-07-11 13:21:01 +0530

    commit

---
+++ src/oop3/Main.java
@@ -0,0 +1,9 @@
+package oop3;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(5);
+        System.out.println(box.l + " " + box.w+ " "+ box.h);
+
+    }
+}
