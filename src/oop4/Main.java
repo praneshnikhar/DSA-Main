81f0c93823dc58d3c66ae5cbe77cdd4b58909548
praneshnikhar <praneshnikhar@gmail.com>
2025-07-15 20:41:22 +0530

    commit

---
+++ src/oop4/Main.java
@@ -0,0 +1,15 @@
+package oop4;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(34, "Kunal");
+
+        //need to do few things
+        //1. access the data members
+        //2. modify the data members
+        obj.getNum();
+        int n = obj.num;
+
+
+    }
+}
