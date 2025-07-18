aaa3fd21a3caab29aa6aafc2099f014575202085
praneshnikhar <praneshnikhar@gmail.com>
2025-07-18 20:56:10 +0530

    commit

---
+++ src/oop4/SubClass.java
@@ -0,0 +1,13 @@
+package oop4;
+
+public class SubClass extends A{
+    public SubClass(int num, String name) {
+        super(num, name);
+        Object
+    }
+
+    public static void main(String[] args) {
+        SubClass obj = new SubClass(45, "pranesh");
+        int n = obj.num;
+    }
+}
