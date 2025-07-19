efafbc79f4fb995a2e4fb534ff769ff0dea899b7
praneshnikhar <praneshnikhar@gmail.com>
2025-07-19 14:31:51 +0530

    commit

--- src/oop5/Parent.java
+++ src/oop5/Parent.java
@@ -2,8 +2,12 @@ package oop5;
 
 public abstract class Parent {
     int age;
+
+    final int VALUE;
+
     public Parent(int age){
         this.age = age;
+        VALUE = 2345;
     }
 
     static void hello(){
